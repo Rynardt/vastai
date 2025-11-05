37d0da0760ef506799cdf74f4491061164467fe8
Rynardt Koen <98612965+Rynardt@users.noreply.github.com>
2025-11-05 12:41:45 +0200

    Update huggingface-cli command to hf download

--- provision.sh
+++ provision.sh
@@ -12,4 +12,4 @@ set -eo pipefail
 wget -P "${WORKSPACE}/HoloCine/checkpoints/HoloCine_dit/full" https://huggingface.co/hlwang06/HoloCine/resolve/main/HoloCine_dit/full/full_high_noise.safetensors
 wget -P "${WORKSPACE}/HoloCine/checkpoints/HoloCine_dit/full" https://huggingface.co/hlwang06/HoloCine/resolve/main/HoloCine_dit/full/full_low_noise.safetensors
 
-huggingface-cli download Wan-AI/Wan2.2-T2V-A14B --local-dir ${WORKSPACE}/checkpoints/Wan2.2-T2V-A14B --allow-patterns "models_t5_*.pth" "Wan2.1_VAE.pth"
+hf download Wan-AI/Wan2.2-T2V-A14B --local-dir ${WORKSPACE}/checkpoints/Wan2.2-T2V-A14B --include "models_t5_*.pth" --include "Wan2.1_VAE.pth"
