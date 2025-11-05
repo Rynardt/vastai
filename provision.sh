d7b9bbc7a02be104587d75d2adc8ce5d4c5b1bfe
Rynardt Koen <98612965+Rynardt@users.noreply.github.com>
2025-11-05 13:10:47 +0200

    Install flash-attention in provision script
    Added installation command for flash-attention.

--- provision.sh
+++ provision.sh
@@ -13,3 +13,5 @@ wget -P "${WORKSPACE}/HoloCine/checkpoints/HoloCine_dit/full" https://huggingfac
 wget -P "${WORKSPACE}/HoloCine/checkpoints/HoloCine_dit/full" https://huggingface.co/hlwang06/HoloCine/resolve/main/HoloCine_dit/full/full_low_noise.safetensors
 
 hf download Wan-AI/Wan2.2-T2V-A14B --local-dir ${WORKSPACE}/checkpoints/Wan2.2-T2V-A14B --include "models_t5_*.pth" --include "Wan2.1_VAE.pth"
+
+python /HoloCine/flash-attention/hopper/setup.py install
