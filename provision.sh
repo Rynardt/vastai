c8e32cd5e47080aedd3fb1e0db7b8e9948a6fb8f
Rynardt Koen <98612965+Rynardt@users.noreply.github.com>
2025-11-07 07:45:31 +0200

    Update provisioning script with new models and packages
    Refactor provisioning script to include new model URLs and package management.

--- provision.sh
+++ provision.sh
@@ -1,17 +1,233 @@
 #!/bin/bash
 
-#
-cd /opt/workspace-internal/
-# Cause the script to exit on failure.
-set -eo pipefail
+source /venv/main/bin/activate
+COMFYUI_DIR=${WORKSPACE}/ComfyUI
 
-# Activate the main virtual environment
-. /venv/main/bin/activate
+# Packages are installed after nodes so we can fix them...
 
-# Download models
-wget -P "${WORKSPACE}/HoloCine/checkpoints/HoloCine_dit/full" https://huggingface.co/hlwang06/HoloCine/resolve/main/HoloCine_dit/full/full_high_noise.safetensors
-wget -P "${WORKSPACE}/HoloCine/checkpoints/HoloCine_dit/full" https://huggingface.co/hlwang06/HoloCine/resolve/main/HoloCine_dit/full/full_low_noise.safetensors
+APT_PACKAGES=(
+    #"package-1"
+    #"package-2"
+)
 
-hf download Wan-AI/Wan2.2-T2V-A14B --local-dir ${WORKSPACE}/checkpoints/Wan2.2-T2V-A14B --include "models_t5_*.pth" --include "Wan2.1_VAE.pth"
+PIP_PACKAGES=(
+    #"package-1"
+    #"package-2"
+)
 
-python /HoloCine/flash-attention/hopper/setup.py install
+NODES=(
+    "https://github.com/Lightricks/ComfyUI-LTXVideo"
+    "https://github.com/kijai/ComfyUI-KJNodes"
+    "https://github.com/aria1th/ComfyUI-LogicUtils"
+    "https://github.com/Mattabyte/ComfyUI-LTXVideo-Registry_Mattabyte"
+    "https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite"
+)
+
+WORKFLOWS=(
+    "https://raw.githubusercontent.com/vast-ai/base-image/refs/heads/comfyui-ltxvideo-provisioning/derivatives/pytorch/derivatives/comfyui/workflows/ltx-video-i2v-simple.json"
+    "https://raw.githubusercontent.com/vast-ai/base-image/refs/heads/comfyui-ltxvideo-provisioning/derivatives/pytorch/derivatives/comfyui/workflows/ltx-video-t2v-simple.json"
+)
+
+INPUT=(
+    "https://comfyanonymous.github.io/ComfyUI_examples/ltxv/island.jpg"
+)
+
+CHECKPOINT_MODELS=(
+)
+
+CLIP_MODELS=(
+)
+
+UNET_MODELS=(
+)
+
+LORA_MODELS=(
+  "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Lightx2v/lightx2v_T2V_14B_cfg_step_distill_v2_lora_rank256_bf16.safetensors"
+)
+
+VAE_MODELS=(
+  "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1_VAE_fp32.safetensors"
+)
+
+ESRGAN_MODELS=(
+)
+
+CONTROLNET_MODELS=(
+)
+
+DIFFUSION_MODELS=(
+  "https://huggingface.co/Kijai/WanVideo_comfy_fp8_scaled/resolve/main/MoCha/Wan2_1_mocha-14B-preview_fp8_e4m3fn_scaled_KJ.safetensors"
+)
+
+TEXTENCODING_MODELS=(
+  "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/umt5-xxl-enc-bf16.safetensors"
+)
+
+### DO NOT EDIT BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###
+
+function provisioning_start() {
+    provisioning_print_header
+    provisioning_get_apt_packages
+    provisioning_update_comfyui
+    provisioning_get_nodes
+    provisioning_get_pip_packages
+    workflows_dir="${COMFYUI_DIR}/user/default/workflows"
+    mkdir -p "${workflows_dir}"
+    provisioning_get_files \
+        "${workflows_dir}" \
+        "${WORKFLOWS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/input" \
+        "${INPUT[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/checkpoints" \
+        "${CHECKPOINT_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/unet" \
+        "${UNET_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/loras" \
+        "${LORA_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/controlnet" \
+        "${CONTROLNET_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/clip" \
+        "${CLIP_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/vae" \
+        "${VAE_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/esrgan" \
+        "${ESRGAN_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/diffusion_models" \
+        "${DIFFUSION_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/text_encoders" \
+        "${TEXTENCODING_MODELS[@]}"
+    provisioning_print_end
+}
+
+function provisioning_get_apt_packages() {
+    if [[ -n $APT_PACKAGES ]]; then
+            sudo $APT_INSTALL ${APT_PACKAGES[@]}
+    fi
+}
+
+function provisioning_get_pip_packages() {
+    if [[ -n $PIP_PACKAGES ]]; then
+            pip install --no-cache-dir ${PIP_PACKAGES[@]}
+    fi
+}
+
+# We must be at release tag v0.3.34 or greater for fp8 support
+provisioning_update_comfyui() {
+    required_tag="v0.3.34"
+    cd ${COMFYUI_DIR}
+    git fetch --all --tags
+    current_commit=$(git rev-parse HEAD)
+    required_commit=$(git rev-parse "$required_tag")
+    if git merge-base --is-ancestor "$current_commit" "$required_commit"; then
+        git checkout "$required_tag"
+        pip install --no-cache-dir -r requirements.txt
+    fi
+}
+
+function provisioning_get_nodes() {
+    for repo in "${NODES[@]}"; do
+        dir="${repo##*/}"
+        path="${COMFYUI_DIR}/custom_nodes/${dir}"
+        requirements="${path}/requirements.txt"
+        if [[ -d $path ]]; then
+            if [[ ${AUTO_UPDATE,,} != "false" ]]; then
+                printf "Updating node: %s...\n" "${repo}"
+                ( cd "$path" && git pull )
+                if [[ -e $requirements ]]; then
+                   pip install --no-cache-dir -r "$requirements"
+                fi
+            fi
+        else
+            printf "Downloading node: %s...\n" "${repo}"
+            git clone "${repo}" "${path}" --recursive
+            if [[ -e $requirements ]]; then
+                pip install --no-cache-dir -r "${requirements}"
+            fi
+        fi
+    done
+}
+
+function provisioning_get_files() {
+    if [[ -z $2 ]]; then return 1; fi
+    
+    dir="$1"
+    mkdir -p "$dir"
+    shift
+    arr=("$@")
+    printf "Downloading %s model(s) to %s...\n" "${#arr[@]}" "$dir"
+    for url in "${arr[@]}"; do
+        printf "Downloading: %s\n" "${url}"
+        provisioning_download "${url}" "${dir}"
+        printf "\n"
+    done
+}
+
+function provisioning_print_header() {
+    printf "\n##############################################\n#                                            #\n#          Provisioning container            #\n#                                            #\n#         This will take some time           #\n#                                            #\n# Your container will be ready on completion #\n#                                            #\n##############################################\n\n"
+}
+
+function provisioning_print_end() {
+    printf "\nProvisioning complete:  Application will start now\n\n"
+}
+
+function provisioning_has_valid_hf_token() {
+    [[ -n "$HF_TOKEN" ]] || return 1
+    url="https://huggingface.co/api/whoami-v2"
+
+    response=$(curl -o /dev/null -s -w "%{http_code}" -X GET "$url" \
+        -H "Authorization: Bearer $HF_TOKEN" \
+        -H "Content-Type: application/json")
+
+    # Check if the token is valid
+    if [ "$response" -eq 200 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function provisioning_has_valid_civitai_token() {
+    [[ -n "$CIVITAI_TOKEN" ]] || return 1
+    url="https://civitai.com/api/v1/models?hidden=1&limit=1"
+
+    response=$(curl -o /dev/null -s -w "%{http_code}" -X GET "$url" \
+        -H "Authorization: Bearer $CIVITAI_TOKEN" \
+        -H "Content-Type: application/json")
+
+    # Check if the token is valid
+    if [ "$response" -eq 200 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Download from $1 URL to $2 file path
+function provisioning_download() {
+    if [[ -n $HF_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?huggingface\.co(/|$|\?) ]]; then
+        auth_token="$HF_TOKEN"
+    elif 
+        [[ -n $CIVITAI_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?civitai\.com(/|$|\?) ]]; then
+        auth_token="$CIVITAI_TOKEN"
+    fi
+    if [[ -n $auth_token ]];then
+        wget --header="Authorization: Bearer $auth_token" -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+    else
+        wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+    fi
+}
+
+# Allow user to disable provisioning if they started with a script they didn't want
+if [[ ! -f /.noprovisioning ]]; then
+    provisioning_start
+fi
